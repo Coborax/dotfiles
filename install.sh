96291ff0c9006d038d1cb6209776a1ab93e64645
Mikkel Mouridsen <mikkelmouridsen@me.com>
2023-04-11 21:12:12 +0200

    Added software install selection

--- install.sh
+++ install.sh
@@ -1,58 +1,122 @@
+# Software Selection
+echo "Install Alacritty? (y/n)"
+read alacritty 
+
+echo "Install Fish? (y/n)"
+read fish 
+
+echo "Install NeoVim? (y/n)"
+read nvim 
+
+echo "Install Yabai? (y/n)"
+read yabai 
+
+echo "Install SketchyBar? (y/n)"
+read sketchybar 
+
+echo "Install skhd? (y/n)"
+read skhd
+
 # Install Brew
 echo "Installing Brew..."
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Adding Brew Taps
 brew tap homebrew/cask-fonts
 
 # Install Brew Formulae
 echo "Installing Brew Formulae..."
-brew install alacritty
-brew install fish
-brew install neovim
-brew install yabai
-brew install sketchybar
+if [[ $alacritty == "y" ]]; then
+    brew install alacritty
+fi
+if [[ $fish == "y" ]]; then
+    brew install fish
+fi
+if [[ $nvim == "y" ]]; then
+    brew install neovim
+fi
+if [[ $yabai == "y" ]]; then
+    brew install yabai
+fi
+if [[ $skhd == "y" ]]; then
+    brew install skhd 
+fi
+if [[ $sketchybar == "y" ]]; then
+    brew install sketchybar
+    # Dependencies for sketchybar config
+    brew install jq
+    brew install gh
+    brew install switchaudio-osx
+fi
+if [[ $nvim == "y" ]]; then
+    brew install nvim 
+fi
 
-# Dependencies for sketchybar config
-# brew tap homebrew/cask-fonts
-brew install jq
-brew install gh
-brew install switchaudio-osx
 
 # Install Brew Casks
 echo "Installing Brew Casks..."
 brew install --cask font-hack-nerd-font
 
-# Dependencies for sketchybar config
-brew install --cask sf-symbols
+if [[ $sketchybar == "y" ]]; then
+    # Dependencies for sketchybar config
+    brew install --cask sf-symbols
+fi
 
 # Installing fonts
 echo "Installing fonts..."
-
-# Dependencies for sketchybar config
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.3/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+if [[ $sketchybar == "y" ]]; then
+    # Dependencies for sketchybar config
+    curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.3/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+fi
 
 # Setup fish shell
-echo "Setting up fish as shell"
-echo "/opt/homebrew/bin/fish" >> /etc/shells
-chsh -s /opt/homebrew/bin/fish
+if [[ $fish == "y" ]]; then
+    echo "Setting up fish as shell"
+    echo "/opt/homebrew/bin/fish" >> /etc/shells
+    chsh -s /opt/homebrew/bin/fish
+fi
 
 # Install AstroVim
-echo "Installing AstroVim"
-git clone --depth 1 "https://github.com/AstroNvim/AstroNvim" "$HOME/.config/nvim"
+if [[ $nvim == "y" ]]; then
+    echo "Installing AstroVim"
+    git clone --depth 1 "https://github.com/AstroNvim/AstroNvim" "$HOME/.config/nvim"
+fi
+
 
 # Create config symlinks
 echo "Creating configurations symlinks..."
-ln -s "$PWD/alacritty" "$HOME/.config"
-ln -s "$PWD/skhd" "$HOME/.config"
-ln -s "$PWD/yabai" "$HOME/.config"
-ln -s "$PWD/fish" "$HOME/.config"
-ln -s "$PWD/sketchybar" "$HOME/.config"
-
-rm -r "$HOME/.config/nvim/lua/user" # Make sure user folder does not exist
-ln -s "$PWD/astronvim_config" "$HOME/.config/nvim/lua/user"
+if [[ $alacritty == "y" ]]; then
+    ln -s "$PWD/alacritty" "$HOME/.config"
+fi
+if [[ $fish == "y" ]]; then
+    ln -s "$PWD/fish" "$HOME/.config"
+fi
+if [[ $nvim == "y" ]]; then
+    brew install neovim
+fi
+if [[ $yabai == "y" ]]; then
+    ln -s "$PWD/yabai" "$HOME/.config"
+fi
+if [[ $skhd == "y" ]]; then
+    ln -s "$PWD/skhd" "$HOME/.config"
+fi
+if [[ $sketchybar == "y" ]]; then
+    ln -s "$PWD/sketchybar" "$HOME/.config"
+fi
+if [[ $nvim == "y" ]]; then
+    rm -r "$HOME/.config/nvim/lua/user" # Make sure user folder does not exist
+    ln -s "$PWD/astronvim_config" "$HOME/.config/nvim/lua/user"
+fi
 
 echo "Starting Services"
-brew services start skhd
-brew services start yabai
-brew services start sketchybar 
+if [[ $yabai == "y" ]]; then
+    brew services start yabai
+fi
+if [[ $skhd == "y" ]]; then
+    brew services start skhd
+fi
+if [[ $sketchybar == "y" ]]; then
+    brew services start sketchybar 
+fi
+
+
